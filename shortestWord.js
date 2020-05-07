80ca8855f286dc8f2560927908992ab3ba3b7aff
Habib Afolabi <habibwritescode@gmail.com>
2020-05-07 12:47:35 +0100

    Add example and make variable names more explicit

--- shortestWord.js
+++ shortestWord.js
@@ -3,9 +3,15 @@
 // Simple, given a string of words, return the length of the shortest word(s).
 // String will never be empty and you do not need to account for different data types.
 
+/*
+Example 
+findShort("bitcoin take over the world maybe who knows perhaps") should return 3 because 'the' and 'who' 
+are the shortest words
+*/
+
 
 // Solution
 
-function findShort(s) {
-    return Math.min(...(s.split(' ').map(str => str.length)));
+function findShort(string) {
+    return Math.min(...(string.split(' ').map(word => word.length)));
 }
